ac748657da0ef5506ded825ce128e17c5fe96042
unknown <vnavnu7@gmail.com>
2024-12-13 16:59:46 +0300

    initalizing login screen

---
+++ lib/logIn.dart
@@ -0,0 +1,119 @@
+
+
+
+
+class LogIn extends StatelessWidget{
+
+const LogIn({super.key});
+
+@override
+Widget build(BuildContext context){
+
+
+return Scaffold(
+
+backgroundColor: const Color(0xFFE5E1DA),
+
+body: Padding(
+              padding: const EdgeInsets.all(20.0),
+
+      child: Column(
+
+      mainAxisAlignment: MainAxisAlignment.center,
+
+      children: [
+
+                  //Email Text Field.
+                  TextField(
+
+                    controller: TextEditingController(), 
+
+                    decoration:const InputDecoration(labelText: 'email', border: OutlineInputBorder()),
+
+                  ),
+
+                  const SizedBox(height: 19,),
+
+                  //Password Text Field.
+                  TextField(
+
+                    controller: TextEditingController(),
+
+                    decoration: const InputDecoration(labelText: 'password', border: OutlineInputBorder()),
+
+                  ),
+
+
+                  const SizedBox(height: 19,),
+
+
+                  //The LogIn button
+                  Row( mainAxisAlignment: MainAxisAlignment.center ,
+
+                       children: [ ElevatedButton(onPressed:  () { },
+
+                                                  style: ElevatedButton.styleFrom(backgroundColor: const Color(0xFF89A8B2), //Background color
+                                                                                  foregroundColor: const Color(0xFFF1F0E8), //Text color
+                                                                                 ),
+
+                                                  child: const Text('LogIn', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),)),
+
+                                                                        
+                                  ],//closing bracket of the children.
+                  
+                      ),
+
+
+                  const SizedBox(height: 19,),
+
+
+                 //The line that includes the link to SignUp screen.
+                  RichText( text:TextSpan( text: "Dont have an account!?", style: const TextStyle(color: Colors.black, fontSize: 17), 
+
+                                           children: [ TextSpan(text: 'Register Here!', 
+                                                                  
+                                                                style: const TextStyle( color: Colors.red, 
+                                                                                        fontWeight: FontWeight.bold, 
+                                                                                        decoration: TextDecoration.underline
+                                                                                      ),
+                                                            
+                                                                recognizer: TapGestureRecognizer() 
+                                                               
+                                                                ..onTap = () {
+
+                                                                              
+                                                                            
+                                                                             },
+
+                                                                ),
+                                                  
+
+                                                      ],//closing bracket of the TextSpan
+                  
+                  
+              
+                                        ),                
+                  
+                  
+                  
+                           ),
+          
+
+
+                ],//closing bracket of the Column-children.
+
+
+
+
+              ),
+ 
+
+            ),
+
+);
+
+
+
+}//closing bracket of the build method.
+
+}//closing bracket of the class.
